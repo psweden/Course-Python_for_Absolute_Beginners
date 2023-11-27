5b4719752e0d52e16a1032d4d633f596a85b6b55
psweden <peter.albertsson63@gmail.com>
2023-11-28 00:22:02 +0100

    Committar matematik variabler och print

---
+++ print.py
@@ -0,0 +1,13 @@
+example_1 = 3.14159
+example_2 = 24601
+example_3 = True
+
+example_5 = 10 + 5
+print(example_5)
+print((4 + 5) * 3)
+
+print(example_1)
+print(example_2)
+print(example_3)
+
+print(2023)
