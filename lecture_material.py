0d83ad1484d477f441edeb2a2806b343471d0fc0
psweden <peter.albertsson63@gmail.com>
2023-11-28 00:12:20 +0100

    Committar matematik variabler och print

--- lecture_material.py
+++ lecture_material.py
@@ -1,1 +1,40 @@
-print("hello Python world!")
+# This is a comment
+add_assign = 5
+add_assign += 7 # add_assign = add_assign + 7 = add_assign = 12
+
+# Subtraction
+sub = 10
+sub -= 5 # 5
+
+# Multiplication
+mult = 10
+mult *= 5 # 50
+
+# division
+div = 10
+div /= 5 # 2.0
+
+# exponentiation
+exp = 10
+exp **= 2 # 100
+
+# floor division
+floor = 10
+floor //= 6 # 1
+
+# modulo
+mod = 10
+mod %= 7 # 3
+
+# Matematiska regler
+# ()
+# **
+# %, /, //, and *
+# + and -
+
+# Expression: (9-7) * 2 ** 3 + 10 % 6 // -1 * 2 - 1
+# Step 1: 2 ** 3 + 10 % 6 // -1 * 2 - 1
+# Step 2: 2 * 8 + 10 % 6 // -1 * 2 - 1
+# Step 3: 16 + -8 - 1
+# Step 4: 7
+
