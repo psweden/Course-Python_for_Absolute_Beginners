ac775f870ce345426e20287ee1cff8a49a9559f1
psweden <peter.albertsson63@gmail.com>
2023-11-29 16:59:00 +0100

    Section 3: String Part 1: Lektion 22-25 Exercise!

---
+++ section-3-exercise-lecture-22-25.py
@@ -0,0 +1,13 @@
+
+# Exercise section 3 String --> lecture 22-25
+
+ex_1 = 3.22
+concat = "is a float"
+
+print(concat + " " + str(ex_1)) # is a float 3.22
+
+ex_2 = "Hello I'm Peter, it's nice to meet you!"
+print("\"Hello, I'm Peter, it's nice to meet you!\"")
+
+# Star
+print("*******\n ***** \n  *** \n   *")
