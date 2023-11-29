a54809a4adba7668f1c327b9584eae8d3233bf4a
psweden <peter.albertsson63@gmail.com>
2023-11-29 15:50:46 +0100

    Section 3: String Part 1: Lektion 20 type() and str() assign datatypes to STRING!

--- section-3-string-lecture-20.py
+++ section-3-string-lecture-20.py
@@ -1,1 +1,16 @@
-print("test")
+ext_1 = False
+ext_2 = 29
+ext_3 = 4.1352
+
+print(type(ext_1)) # Skriver ut datatypen = bool
+print(type(ext_2)) # Skriver ut datatypen = int
+print(type(ext_3)) # Skriver ut datatypen = float
+
+# str() 'string function' --> ASSIGN to STRING
+ex_4 = str(True)
+ex_5 = str(5)
+ex_6 = str(3.21)
+
+print(type(ex_4))
+print(type(ex_5))
+print(type(ex_6))
