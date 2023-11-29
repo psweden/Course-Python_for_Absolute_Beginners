446777481ab54761825c30ce0c41067fda50b66a
psweden <peter.albertsson63@gmail.com>
2023-11-29 16:03:37 +0100

    Section 3: String Part 1: Lektion 21 Escape Sequences

---
+++ section-3-string-lecture-21.py
@@ -0,0 +1,11 @@
+# section 3 Escape Sequences
+# \t = tabb
+# \n = new line
+
+print("This\tis\ta\tlot\tof\tspace")    # tabb = \t
+print("Line one\nLine two") # new line = \n
+
+print('"When I said \'immediately,\' I ment today!" said King Saul.')
+print("\"Do or do not. There is no try.\"")
+
+print("All escapte sequences contain a \\.")
