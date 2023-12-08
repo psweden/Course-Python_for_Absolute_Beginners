cac6240483fcdc41d16c29fa8da496d4563df90e
psweden <peter.albertsson63@gmail.com>
2023-12-08 14:21:24 +0100

    Section 3: String Part 1: Lektion 29-31 int() and float() with exercise!

---
+++ section-3-exercise-lecture-29.py
@@ -0,0 +1,9 @@
+user_int = int(input("Please enter a integer."))
+print(user_int)
+print(type(user_int))
+
+
+user_float = float(input("Please enter a integer."))
+print(user_float)
+print(type(user_float))
+
