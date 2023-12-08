abd7cc93bddb69a7ea0d0e205b22bb68e1869c28
psweden <peter.albertsson63@gmail.com>
2023-12-08 14:10:07 +0100

    Section 3: String Part 1: Lektion 26-28 Input with exercise

---
+++ section-3-string-lecture-26.py
@@ -0,0 +1,14 @@
+# Section 3: String Part 1: Lektion 26 input()
+
+name = input("Please enter your name. ")
+print("Your name is " + name + ".")
+print(type(name))
+
+quest = input("what is your quest? ")
+print("Your quest is " + quest + ".")
+print(type(quest))
+
+fav_color = input("What is your favorite color?")
+print("Your favorite color is " + fav_color + ".")
+print(type(fav_color))
+
